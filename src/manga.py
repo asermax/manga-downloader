c1e20cd711576de5a2c4030bfdf8bc24659a6480
Agustin Carrasco <asermax@gmail.com>
2013-01-09 03:30:28 -0300

    Some modifications on the classes of the manga module. This should be enough to contain data of either local or remote mangas.

--- src/manga.py
+++ src/manga.py
@@ -18,14 +18,16 @@
 
 class Page(object):
 
-    def __init__(self, filename, number=0):
-        self.filename = filename
+    def __init__(self, number, path_chunk=None, extension=None):
         self.number = number
+        self.path_chunk = path_chunk if path_chunk else str(number)
+        self.extension = extension if extension else 'jpg'
 
 class Chapter(object):
 
-    def __init__(self, number):
+    def __init__(self, number, path_chunk=None):
         self.number = number
+        self.path_chunk = path_chunk if path_chunk else ('c%d' % number)
         self._pages = []
 
     @property
@@ -37,18 +39,19 @@ class Chapter(object):
     def page_count(self):
         return len(self._pages)
 
-    def add_page(self, filename, number=None):
+    def add_page(self, number=None, page_path=None, extension=None):
         if not number:
             number = max([page.number for page in self._pages])
 
-        page = Page(filename, number)
+        page = Page(number, page_path, extension)
         self._pages.append(page)
 
         return page
 
 class Volume(object):
 
-    def __init__(self, number):
+    def __init__(self, number, path_chunk=None):
+        self.path_chunk = path_chunk if path_chunk else ('v%d' % number)
         self.number = number
         self._chapters = []
 
@@ -61,18 +64,20 @@ class Volume(object):
     def chapter_count(self):
         return len(self._chapters)
 
-    def add_chapter(self, number=None):
+    def add_chapter(self, number=None, chapter_path=None):
         if not number:
             number = max([chapter.number for chapter in self._chapters])
 
-        chapter = Chapter(number)
+        chapter = Chapter(number, chapter_path)
         self._chapters.append(chapter)
 
         return chapter
 
+
 class Manga(object):
 
-    def __init__(self, name):
+    def __init__(self, name, base_path):
+        self.path_chunk = base_path if base_path else ('./%s' % name)
         self.name = name
         self._volumes = []
 
@@ -85,11 +90,12 @@ class Manga(object):
     def volume_count(self):
         return len(self._volumes)
 
-    def add_volume(self, number=None):
+    def add_volume(self, number=None, volume_path=None):
         if not number:
             number = max([volume.number for volume in self._volumes])
 
-        volume = Volume(number)
+        volume = Volume(number, volume_path)
         self._volumes.append(volume)
 
         return volume
+
